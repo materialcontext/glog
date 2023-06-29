6b5ba6bfaa58e4602a5a40daec00c560538a6be4
materialcontext <materialconditionsblog@gmail.com>
2023-06-29 21:05:21 +0900

    safdsdf

--- module/documents/actor.mjs
+++ module/documents/actor.mjs
@@ -1,71 +1,71 @@
 export class PlayerCharacter extends Actor {
-    
-    /** @override */
-    prepareData() {
-        super.prepareData();
-    };
+  /** @override */
+  prepareData() {
+    super.prepareData();
+  }
 
-    /** @override */
-    prepareBaseData() {};
+  /** @override */
+  prepareBaseData() {}
 
-    /** @override */
-    prepareDerivedData() {
-        const actorData = this;
-        const systemData = actorData.system;
-        const flags = actorData.flags.glog || {};
+  /** @override */
+  prepareDerivedData() {
+    const actorData = this;
+    const systemData = actorData.system;
+    const flags = actorData.flags.glog || {};
 
-        this._preparePlayerCharacterData(actorData);
-        this._prepareNPCData(actorData);
-    };
-    
-    // prepare playerCharacter type specific data
-    _preparePlayerCharacterData(actorData) {
-        if (actorData.type !== 'playerCharacter') return;
+    this._preparePlayerCharacterData(actorData);
+    this._prepareNPCData(actorData);
+  }
 
-        //make data modifications here
-    };
+  // prepare playerCharacter type specific data
+  _preparePlayerCharacterData(actorData) {
+    if (actorData.type !== "playerCharacter") return;
 
-    // prepare NPC type specific data
-    _prepareNPCData(actorData) {
-        if (actorData.type !== 'npc') return;
-    };
+    // Make modifications to data here. For example:
+    const systemData = actorData.system;
+  }
 
-    // prepare companion specific data
-    _prepareCompanionData(actorData) {
-        if (actorData.type !== 'companion') return;
-    };
-    
-    // prepare hireling specific data
-    _prepareHirelingData(actorData) {
-        if (actorData.type !== 'hireling') return;
-    };
+  // prepare NPC type specific data
+  _prepareNPCData(actorData) {
+    if (actorData.type !== "npc") return;
+  }
 
-    /**
-     * Override getRollData() that's supplied to rolls
-     */
-    getRollData() {
-        const data = super.getRollData();
+  // prepare companion specific data
+  _prepareCompanionData(actorData) {
+    if (actorData.type !== "companion") return;
+  }
 
-        //prepare character roll data
-        this._getCharacterRollData(data);
+  // prepare hireling specific data
+  _prepareHirelingData(actorData) {
+    if (actorData.type !== "hireling") return;
+  }
 
-        return data;
-    };
+  /**
+   * Override getRollData() that's supplied to rolls
+   */
+  getRollData() {
+    const data = super.getRollData();
 
-    _getCharacterRollData(data) {
-        if (this.type !== "playerCharacter" || this.type !== "hireling") {
-            return;
-        }
-        // add ability data to top level for access like `@str + 4`
-        if (data.abilities) {
-            for (let [k, v] of Object.entries(data.abilities)) {
-                data[k] = foundry.utils.deepClone(v);
-            };
-        };
+    //prepare character roll data
+    this._getCharacterRollData(data);
 
-        // do the same for level or fallback to 0
-        if (data.attributes.level) {
-            data.lvl = data.attributes.level.value ?? 0;
-        };
-    };
-};
+    return data;
+  }
+
+  _getCharacterRollData(data) {
+    if (this.type !== "playerCharacter" || this.type !== "hireling") {
+      return;
+    }
+    // add ability data to top level for access like `@str + 4`
+    if (data.abilities) {
+      for (let [k, v] of Object.entries(data.abilities)) {
+        data[k] = foundry.utils.deepClone(v);
+      }
+    }
+
+    // do the same for level or fallback to 0
+    if (data.attributes.level) {
+      data.lvl = data.attributes.level.value ?? 0;
+    }
+  }
+}
