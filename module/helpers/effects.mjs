7b61b63c78e910ff7030beeb87f6edf06817dd21
materialcontext <materialconditionsblog@gmail.com>
2023-07-04 15:21:16 +0900

    sdasdw

--- module/helpers/effects.mjs
+++ module/helpers/effects.mjs
@@ -65,3 +65,20 @@ export function prepareActiveEffectCategories(effects) {
   }
   return categories;
 }
+
+export function collapsible() {
+  var coll = document.getElementsByClassName("collapsible");
+  var i;
+
+  for (i = 0; i < coll.length; i++) {
+      coll[i].addEventListener("click", function() {
+          this.classList.toggle("collapsible");
+          var itemdescription = this.nextElementSibling;
+          if (itemdescription.style.display === "block") {
+              itemdescription.style.display = "none";
+          } else {
+              itemdescription.style.display = "block";
+          }
+      });
+  }
+}
