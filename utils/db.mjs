d8a2771e599ce5a6951f3d0b4ffa491834173665
materialcontext <materialconditionsblog@gmail.com>
2023-07-20 11:08:39 +0900

    sdfsdf

--- utils/db.mjs
+++ utils/db.mjs
@@ -1,17 +1,27 @@
 import  { Level } from 'level';
-import fighter from '../packs/src/fighter.json' assert { type: "json" };
+import { readdir, readFile } from 'node:fs';
+import { resolve } from 'path';
 
-// Create a database
-const db = new Level('packs/classes', { valueEncoding: 'json' });
-const source = '../packs/src'
+const dir = resolve('./');
 
-const classes = ['fighter'];
-// Add an entry with a clas as the key and its json as the value
-await db.put(classes[0], fighter);
+async function buildDB(dirName) {
+    // Create a database
+    const db = new Level(`packs/${dirName}`, { valueEncoding: 'json' });
+
+    // add a db entry for each file in the directory, with the filename as the key and the json content as the value
+    readdir(dir + `/packs/src/${dirName}`, (err, files) => {
+        if (err) throw(err);
+        files.forEach(file => {
+            const filepath = dir + `/packs/src/${dirName}/` + file;
+            readFile(filepath, (err, data) => {
+                if (err) throw(err);
+                 db.put(file.replace('.json', ''), JSON.parse(data));
+            }); 
+        });
+    });
+};
+
+await buildDB('classes');
 
 // Add multiple entries
 // await db.batch([{ type: 'put', key: 'b', value: 2 }])
-
-// Get value of key 'a': 1
-const value = await db.get('fighter');
-console.log(value);
